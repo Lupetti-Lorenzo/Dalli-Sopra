8be20ed846cd79ab434b66215c6d487f8bd09adc
Lorenzo Lupetti <lorenzo.lupetti3@gmail.com>
2021-07-30 12:02:58 +0200

    Eliminated login and database connection files

--- src/scripts/db_config.script.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('DB_NAME', 'Dalli');
-define('DB_USER', 'Client');
-define('DB_PASSWORD', 'Client');
-define('DB_HOST', '127.0.0.1');
-
-$pdo = new PDO("mysql:host=" . DB_HOST . "; dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
